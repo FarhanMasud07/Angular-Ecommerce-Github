11fcbd440ec5286d486029dc418d7f93a1f9a4d4
FarhanNitrogen <farhan.masud@selise.ch>
2021-02-04 01:42:44 +0600

    Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LaligaComponent } from './laliga/laliga.component';
+import { GithubComponent } from './github/github.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { HomeComponent } from './ecommerce/home/home.component';
+import { ManagementComponent } from './ecommerce/management/management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'laliga', component: LaligaComponent},
+  {path: 'github', component: GithubComponent},
+  {path: 'management', component: ManagementComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routeComponent = [LaligaComponent, GithubComponent, EcommerceComponent, HomeComponent, ManagementComponent];
